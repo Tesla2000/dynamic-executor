513ec78111cd2355afeab3e5ec1c45719889474e
Tesla2000 <fratajczak124@gmail.com>
2023-11-22 19:21:11 +0100

    windows is supported

--- src/dynthon/__init__.py
+++ src/dynthon/__init__.py
@@ -1,3 +1,7 @@
-__all__ = ["DynamicClass"]
+__all__ = [
+    "DynamicClass",
+    "exec_in_dynamic_mode"
+]
 
 from .classes.DynamicClassModule import DynamicClass
+from .utils.exec_in_dynamic_mode import exec_in_dynamic_mode
--- src/dynthon/utils/get_modules.py
+++ src/dynthon/utils/get_modules.py
@@ -1,18 +1,20 @@
 import re
 import sys
+import platform
 
+venv_module = f"python{'.'.join(sys.version.split('.')[:2])}" if platform.system() == 'Linux' else f"Python{''.join(sys.version.split('.')[:2])}"
 
 def get_modules():
     return dict(
         (variable, value)
         for variable, value in sys.modules.items()
         if not re.findall(r"module \'[^\']+\' \((?:built-in|frozen)\)", str(value))
-        and "python3.12" not in str(value)
+        and venv_module not in str(value)
         and not variable.startswith("_")
         and not any(
             map(
                 variable.__contains__,
-                ("dynthon", "pyexpat", "pydev", "xml.parsers.expat.", "typing."),
+                ("dynthon", "pyexpat", "pydev", "xml.parsers.expat.", "typing.", 'cython_runtime'),
             )
         )
     )
